0f20b7d4fd8a19a66e0584a9847f7e8da09900fb
Mr-Filatik <filatov23wlad@gmail.com>
2025-03-21 10:36:16 +0400

    task-001: refactoring test for scanner

--- internal/basics/structures/alignment/scanner/scanner_test.go
+++ internal/basics/structures/alignment/scanner/scanner_test.go
@@ -7,94 +7,61 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestScanStructInfo(t *testing.T) {
-	testInstance(t)
-	testPointerInstance(t)
-	testArrayInstance(t)
-	testNestedStruct(t)
-	testEmptyStruct(t)
-	testSingleFieldStruct(t)
-	testBoolStruct(t)
-	testOverlapStruct(t)
-}
+type emptyStruct struct{}
 
-type TestStruct struct {
-	Field1 int8
-	Field2 int16
-	Field3 int32
-	Field4 int64
-	Field5 int16
-}
+func TestScanStructInfo_EmptyStruct(t *testing.T) {
+	testInstance := emptyStruct{}
+	result := ScanStructInfo(testInstance, "TestScanStructInfo_EmptyStruct")
 
-func testInstance(t *testing.T) {
-	testInstance := TestStruct{}
-
-	result := ScanStructInfo(testInstance, "TestStruct")
-
-	// Ожидаемый результат
 	expected := entity.StructInfo{
-		StructName:  "TestStruct",
-		StructType:  "TestStruct",
-		StructSize:  24,
+		StructName:  "TestScanStructInfo_EmptyStruct",
+		StructType:  "emptyStruct",
+		StructSize:  0,
+		StructAlign: 8,
+		FieldInfos:  []entity.FieldInfo{},
+	}
+	assert.Equal(t, expected, result)
+}
+
+type singleFieldStruct struct {
+	Field1 int64
+}
+
+func TestScanStructInfo_SingleFieldStruct(t *testing.T) {
+	testInstance := singleFieldStruct{}
+	result := ScanStructInfo(testInstance, "TestScanStructInfo_SingleFieldStruct")
+
+	expected := entity.StructInfo{
+		StructName:  "TestScanStructInfo_SingleFieldStruct",
+		StructType:  "singleFieldStruct",
+		StructSize:  8,
 		StructAlign: 8,
 		FieldInfos: []entity.FieldInfo{
 			{
 				FieldName:    "Field1",
-				FieldType:    "int8",
-				FieldOffset:  0,
-				FieldSize:    1,
-				FieldPadding: 1,
-			},
-			{
-				FieldName:    "Field2",
-				FieldType:    "int16",
-				FieldOffset:  2,
-				FieldSize:    2,
-				FieldPadding: 0,
-			},
-			{
-				FieldName:    "Field3",
-				FieldType:    "int32",
-				FieldOffset:  4,
-				FieldSize:    4,
-				FieldPadding: 0,
-			},
-			{
-				FieldName:    "Field4",
 				FieldType:    "int64",
-				FieldOffset:  8,
+				FieldOffset:  0,
 				FieldSize:    8,
 				FieldPadding: 0,
 			},
-			{
-				FieldName:    "Field5",
-				FieldType:    "int16",
-				FieldOffset:  16,
-				FieldSize:    2,
-				FieldPadding: 6,
-			},
 		},
 	}
-
-	// Проверяем, что результат совпадает с ожидаемым
 	assert.Equal(t, expected, result)
 }
 
-type TestPointerStruct struct {
+type pointerStruct struct {
 	Field1 int32
 	Field2 *int
 	Field3 int8
 }
 
-func testPointerInstance(t *testing.T) {
-	testInstance := TestPointerStruct{}
+func TestScanStructInfo_PointerStruct(t *testing.T) {
+	testInstance := pointerStruct{}
+	result := ScanStructInfo(testInstance, "TestScanStructInfo_PointerStruct")
 
-	result := ScanStructInfo(testInstance, "TestPointerStruct")
-
-	// Ожидаемый результат
 	expected := entity.StructInfo{
-		StructName:  "TestPointerStruct",
-		StructType:  "TestPointerStruct",
+		StructName:  "TestScanStructInfo_PointerStruct",
+		StructType:  "pointerStruct",
 		StructSize:  24,
 		StructAlign: 8,
 		FieldInfos: []entity.FieldInfo{
@@ -121,26 +88,22 @@ func testPointerInstance(t *testing.T) {
 			},
 		},
 	}
-
-	// Проверяем, что результат совпадает с ожидаемым
 	assert.Equal(t, expected, result)
 }
 
-type ArrayStruct struct {
+type arrayStruct struct {
 	Field1 [3]int16
 	Field2 int32
 	Field3 int8
 }
 
-func testArrayInstance(t *testing.T) {
-	testInstance := ArrayStruct{}
+func TestScanStructInfo_ArrayStruct(t *testing.T) {
+	testInstance := arrayStruct{}
+	result := ScanStructInfo(testInstance, "TestScanStructInfo_ArrayStruct")
 
-	result := ScanStructInfo(testInstance, "TestArrayStruct")
-
-	// Ожидаемый результат
 	expected := entity.StructInfo{
-		StructName:  "TestArrayStruct",
-		StructType:  "ArrayStruct",
+		StructName:  "TestScanStructInfo_ArrayStruct",
+		StructType:  "arrayStruct",
 		StructSize:  16,
 		StructAlign: 8,
 		FieldInfos: []entity.FieldInfo{
@@ -167,33 +130,71 @@ func testArrayInstance(t *testing.T) {
 			},
 		},
 	}
-
-	// Проверяем, что результат совпадает с ожидаемым
 	assert.Equal(t, expected, result)
 }
 
-type NestedStructInner struct {
+type sliceStruct struct {
+	Field1 []int32
+	Field2 int32
+	Field3 int8
+}
+
+func TestScanStructInfo_SliceStruct(t *testing.T) {
+	testInstance := sliceStruct{}
+	result := ScanStructInfo(testInstance, "TestScanStructInfo_SliceStruct")
+
+	expected := entity.StructInfo{
+		StructName:  "TestScanStructInfo_SliceStruct",
+		StructType:  "sliceStruct",
+		StructSize:  32,
+		StructAlign: 8,
+		FieldInfos: []entity.FieldInfo{
+			{
+				FieldName:    "Field1",
+				FieldType:    "",
+				FieldOffset:  0,
+				FieldSize:    24,
+				FieldPadding: 0,
+			},
+			{
+				FieldName:    "Field2",
+				FieldType:    "int32",
+				FieldOffset:  24,
+				FieldSize:    4,
+				FieldPadding: 0,
+			},
+			{
+				FieldName:    "Field3",
+				FieldType:    "int8",
+				FieldOffset:  28,
+				FieldSize:    1,
+				FieldPadding: 3,
+			},
+		},
+	}
+	assert.Equal(t, expected, result)
+}
+
+type nestedInnerStruct struct {
 	InnerField1 int8
 	InnerField2 int32
 }
 
-type NestedStructOuter struct {
+type nestedOuterStruct struct {
 	Field1 int16
-	Field2 NestedStructInner
+	Field2 nestedInnerStruct
 	Field3 int8
 }
 
-func testNestedStruct(t *testing.T) {
-	testInstance := NestedStructOuter{
-		Field2: NestedStructInner{},
+func TestScanStructInfo_NestedStruct(t *testing.T) {
+	testInstance := nestedOuterStruct{
+		Field2: nestedInnerStruct{},
 	}
+	result := ScanStructInfo(testInstance, "TestScanStructInfo_NestedStruct")
 
-	result := ScanStructInfo(testInstance, "TestNestedStruct")
-
-	// Ожидаемый результат
 	expected := entity.StructInfo{
-		StructName:  "TestNestedStruct",
-		StructType:  "NestedStructOuter",
+		StructName:  "TestScanStructInfo_NestedStruct",
+		StructType:  "nestedOuterStruct",
 		StructSize:  16,
 		StructAlign: 8,
 		FieldInfos: []entity.FieldInfo{
@@ -206,7 +207,7 @@ func testNestedStruct(t *testing.T) {
 			},
 			{
 				FieldName:    "Field2",
-				FieldType:    "NestedStructInner",
+				FieldType:    "nestedInnerStruct",
 				FieldOffset:  4,
 				FieldSize:    8,
 				FieldPadding: 0,
@@ -220,50 +221,6 @@ func testNestedStruct(t *testing.T) {
 			},
 		},
 	}
-
-	// Проверяем, что результат совпадает с ожидаемым
-	assert.Equal(t, expected, result)
-}
-
-type emptyStruct struct{}
-
-func testEmptyStruct(t *testing.T) {
-	testInstance := emptyStruct{}
-	result := ScanStructInfo(testInstance, "TestEmptyStruct")
-	expected := entity.StructInfo{
-		StructName:  "TestEmptyStruct",
-		StructType:  "emptyStruct",
-		StructSize:  0,
-		StructAlign: 8,
-		FieldInfos:  []entity.FieldInfo{},
-	}
-	// Проверяем, что результат совпадает с ожидаемым
-	assert.Equal(t, expected, result)
-}
-
-type singleFieldStruct struct {
-	Field1 int64
-}
-
-func testSingleFieldStruct(t *testing.T) {
-	testInstance := singleFieldStruct{}
-	result := ScanStructInfo(testInstance, "TestSingleFieldStruct")
-	expected := entity.StructInfo{
-		StructName:  "TestSingleFieldStruct",
-		StructType:  "singleFieldStruct",
-		StructSize:  8,
-		StructAlign: 8,
-		FieldInfos: []entity.FieldInfo{
-			{
-				FieldName:    "Field1",
-				FieldType:    "int64",
-				FieldOffset:  0,
-				FieldSize:    8,
-				FieldPadding: 0,
-			},
-		},
-	}
-	// Проверяем, что результат совпадает с ожидаемым
 	assert.Equal(t, expected, result)
 }
 
@@ -273,11 +230,12 @@ type boolStruct struct {
 	Field3 bool
 }
 
-func testBoolStruct(t *testing.T) {
+func TestScanStructInfo_BoolStruct(t *testing.T) {
 	testInstance := boolStruct{}
-	result := ScanStructInfo(testInstance, "TestBoolStruct")
+	result := ScanStructInfo(testInstance, "TestScanStructInfo_BoolStruct")
+
 	expected := entity.StructInfo{
-		StructName:  "TestBoolStruct",
+		StructName:  "TestScanStructInfo_BoolStruct",
 		StructType:  "boolStruct",
 		StructSize:  6,
 		StructAlign: 8,
@@ -305,7 +263,48 @@ func testBoolStruct(t *testing.T) {
 			},
 		},
 	}
-	// Проверяем, что результат совпадает с ожидаемым
+	assert.Equal(t, expected, result)
+}
+
+type stringStruct struct {
+	Field1 int64
+	Field2 string
+	Field3 int32
+}
+
+func TestScanStructInfo_StringStruct(t *testing.T) {
+	testInstance := stringStruct{}
+	result := ScanStructInfo(testInstance, "TestScanStructInfo_StringStruct")
+
+	expected := entity.StructInfo{
+		StructName:  "TestScanStructInfo_StringStruct",
+		StructType:  "stringStruct",
+		StructSize:  32,
+		StructAlign: 8,
+		FieldInfos: []entity.FieldInfo{
+			{
+				FieldName:    "Field1",
+				FieldType:    "int64",
+				FieldOffset:  0,
+				FieldSize:    8,
+				FieldPadding: 0,
+			},
+			{
+				FieldName:    "Field2",
+				FieldType:    "string",
+				FieldOffset:  8,
+				FieldSize:    16,
+				FieldPadding: 0,
+			},
+			{
+				FieldName:    "Field3",
+				FieldType:    "int32",
+				FieldOffset:  24,
+				FieldSize:    4,
+				FieldPadding: 4,
+			},
+		},
+	}
 	assert.Equal(t, expected, result)
 }
 
@@ -315,11 +314,12 @@ type overlapStruct struct {
 	Field3 int64
 }
 
-func testOverlapStruct(t *testing.T) {
+func TestScanStructInfo_OverlapStruct(t *testing.T) {
 	testInstance := overlapStruct{}
-	result := ScanStructInfo(testInstance, "TestOverlapStruct")
+	result := ScanStructInfo(testInstance, "TestScanStructInfo_OverlapStruct")
+
 	expected := entity.StructInfo{
-		StructName:  "TestOverlapStruct",
+		StructName:  "TestScanStructInfo_OverlapStruct",
 		StructType:  "overlapStruct",
 		StructSize:  16,
 		StructAlign: 8,
@@ -347,7 +347,48 @@ func testOverlapStruct(t *testing.T) {
 			},
 		},
 	}
-	// Проверяем, что результат совпадает с ожидаемым
+	assert.Equal(t, expected, result)
+}
+
+type aligned8Struct struct {
+	Field1 int32
+	Field2 int32
+	Field3 int8
+}
+
+func TestScanStructInfo_Aligned8Struct(t *testing.T) {
+	testInstance := aligned8Struct{}
+	result := ScanStructInfo(testInstance, "TestScanStructInfo_Aligned8Struct")
+
+	expected := entity.StructInfo{
+		StructName:  "TestScanStructInfo_Aligned8Struct",
+		StructType:  "aligned8Struct",
+		StructSize:  12,
+		StructAlign: 8,
+		FieldInfos: []entity.FieldInfo{
+			{
+				FieldName:    "Field1",
+				FieldType:    "int32",
+				FieldOffset:  0,
+				FieldSize:    4,
+				FieldPadding: 0,
+			},
+			{
+				FieldName:    "Field2",
+				FieldType:    "int32",
+				FieldOffset:  4,
+				FieldSize:    4,
+				FieldPadding: 0,
+			},
+			{
+				FieldName:    "Field3",
+				FieldType:    "int8",
+				FieldOffset:  8,
+				FieldSize:    1,
+				FieldPadding: 3,
+			},
+		},
+	}
 	assert.Equal(t, expected, result)
 }
 
@@ -357,10 +398,40 @@ type aligned16Struct struct {
 	Field3 int8
 }
 
-type aligned8Struct struct {
-	Field1 int32
-	Field2 int32
-	Field3 int8
+func TestScanStructInfo_Aligned16Struct(t *testing.T) {
+	testInstance := aligned16Struct{}
+	result := ScanStructInfo(testInstance, "TestScanStructInfo_Aligned16Struct")
+
+	expected := entity.StructInfo{
+		StructName:  "TestScanStructInfo_Aligned16Struct",
+		StructType:  "aligned16Struct",
+		StructSize:  24,
+		StructAlign: 8,
+		FieldInfos: []entity.FieldInfo{
+			{
+				FieldName:    "Field1",
+				FieldType:    "int64",
+				FieldOffset:  0,
+				FieldSize:    8,
+				FieldPadding: 0,
+			},
+			{
+				FieldName:    "Field2",
+				FieldType:    "int64",
+				FieldOffset:  8,
+				FieldSize:    8,
+				FieldPadding: 0,
+			},
+			{
+				FieldName:    "Field3",
+				FieldType:    "int8",
+				FieldOffset:  16,
+				FieldSize:    1,
+				FieldPadding: 7,
+			},
+		},
+	}
+	assert.Equal(t, expected, result)
 }
 
 type unorderedStruct struct {
@@ -369,14 +440,130 @@ type unorderedStruct struct {
 	Field3 int16
 }
 
+func TestScanStructInfo_UnorderedStruct(t *testing.T) {
+	testInstance := unorderedStruct{}
+	result := ScanStructInfo(testInstance, "TestScanStructInfo_UnorderedStruct")
+
+	expected := entity.StructInfo{
+		StructName:  "TestScanStructInfo_UnorderedStruct",
+		StructType:  "unorderedStruct",
+		StructSize:  24,
+		StructAlign: 8,
+		FieldInfos: []entity.FieldInfo{
+			{
+				FieldName:    "Field1",
+				FieldType:    "int8",
+				FieldOffset:  0,
+				FieldSize:    1,
+				FieldPadding: 7,
+			},
+			{
+				FieldName:    "Field2",
+				FieldType:    "int64",
+				FieldOffset:  8,
+				FieldSize:    8,
+				FieldPadding: 0,
+			},
+			{
+				FieldName:    "Field3",
+				FieldType:    "int16",
+				FieldOffset:  16,
+				FieldSize:    2,
+				FieldPadding: 6,
+			},
+		},
+	}
+	assert.Equal(t, expected, result)
+}
+
+type orderedStruct struct {
+	Field1 int8
+	Field2 int16
+	Field3 int64
+}
+
+func TestScanStructInfo_OrderedStruct(t *testing.T) {
+	testInstance := orderedStruct{}
+	result := ScanStructInfo(testInstance, "TestScanStructInfo_OrderedStruct")
+
+	expected := entity.StructInfo{
+		StructName:  "TestScanStructInfo_OrderedStruct",
+		StructType:  "orderedStruct",
+		StructSize:  16,
+		StructAlign: 8,
+		FieldInfos: []entity.FieldInfo{
+			{
+				FieldName:    "Field1",
+				FieldType:    "int8",
+				FieldOffset:  0,
+				FieldSize:    1,
+				FieldPadding: 1,
+			},
+			{
+				FieldName:    "Field2",
+				FieldType:    "int16",
+				FieldOffset:  2,
+				FieldSize:    2,
+				FieldPadding: 4,
+			},
+			{
+				FieldName:    "Field3",
+				FieldType:    "int64",
+				FieldOffset:  8,
+				FieldSize:    8,
+				FieldPadding: 0,
+			},
+		},
+	}
+	assert.Equal(t, expected, result)
+}
+
 type largeTypeStruct struct {
 	Field1 int64
 	Field2 float64
 	Field3 int32
+	Field4 bool
 }
 
-type stringStruct struct {
-	Field1 int64
-	Field2 string
-	Field3 int32
+func TestScanStructInfo_LargeTypeStruct(t *testing.T) {
+	testInstance := largeTypeStruct{}
+	result := ScanStructInfo(testInstance, "TestScanStructInfo_LargeTypeStruct")
+
+	expected := entity.StructInfo{
+		StructName:  "TestScanStructInfo_LargeTypeStruct",
+		StructType:  "largeTypeStruct",
+		StructSize:  24,
+		StructAlign: 8,
+		FieldInfos: []entity.FieldInfo{
+			{
+				FieldName:    "Field1",
+				FieldType:    "int64",
+				FieldOffset:  0,
+				FieldSize:    8,
+				FieldPadding: 0,
+			},
+			{
+				FieldName:    "Field2",
+				FieldType:    "float64",
+				FieldOffset:  8,
+				FieldSize:    8,
+				FieldPadding: 0,
+			},
+			{
+				FieldName:    "Field3",
+				FieldType:    "int32",
+				FieldOffset:  16,
+				FieldSize:    4,
+				FieldPadding: 0,
+			},
+			{
+				FieldName:    "Field4",
+				FieldType:    "bool",
+				FieldOffset:  20,
+				FieldSize:    1,
+				FieldPadding: 3,
+			},
+		},
+	}
+	assert.Equal(t, expected, result)
 }
